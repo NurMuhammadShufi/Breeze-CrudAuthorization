f9308c9a5258056750feda1138ecb384904b829c
Nur Muhammad Shufi <shufimuhammad013@gmail.com>
2023-03-27 01:21:00 +0700

    Setup Project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="card bg-white">
+                <div class="card-body">
+                    <form action="" method="POST">
+                        @csrf
+                        <textarea name="" rows="3" class="textarea textarea-bordered w-full bg-white text-slate-800"
+                            placeholder="apa yang anda pikirkan?"></textarea>
+                        <input type="submit" value="Tweet" class="btn btn-primary">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
